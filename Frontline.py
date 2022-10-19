74bc6610e6384bf357807353f0986a10e9027b35
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2022-10-19 23:16:45 +0200

    Sprites, background

--- Frontline.py
+++ Frontline.py
@@ -8,8 +8,12 @@ HEIGHT = 800
 FPS = 60 
 
 #Colors 
-Grey = (192, 192, 192)
-Red = (255, 0, 0)
+GREY = (192, 192, 192)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0 , 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
 
 #Pygame initialiser, Music, Screen ratio, Title game, 
 pygame.init()
@@ -18,8 +22,20 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Frontline')
 clock = pygame.time.Clock 
 
+# Display title icon 
+#icon = pygame.image.load('jet.png')
+#pygame.display.set_icon(icon)
+
+#Game Loop, Running speed
 running = True
 while running:
+    clock.tick(FPS)
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill(GREY)
+    
+    pygame.display.flip()
 
-
-
+pygame.quit()
