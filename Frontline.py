31d8080542f2893d7c42e333fdbf6884b2ee7b92
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2022-10-19 22:45:22 +0200

    Setup game

--- Frontline.py
+++ Frontline.py
@@ -1,1 +1,25 @@
-## test
+#Frontline 
+import pygame
+import random 
+import os 
+
+WIDTH = 1000
+HEIGHT = 800 
+FPS = 60 
+
+#Colors 
+Grey = (192, 192, 192)
+Red = (255, 0, 0)
+
+#Pygame initialiser, Music, Screen ratio, Title game, 
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Frontline')
+clock = pygame.time.Clock 
+
+running = True
+while running:
+
+
+
